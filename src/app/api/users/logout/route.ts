d2831b60a3dd7bd7563a020c011ad198e94b59cc
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-27 14:53:13 +0530

    misc commit

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
+    const response = NextResponse.json({
+      message: 'Logged out successfully',
+    });
+
+    response.cookies.set('token', '', { expires: new Date(0) });
+    return response;
   } catch (error) {
     return NextResponse.json(
       {
