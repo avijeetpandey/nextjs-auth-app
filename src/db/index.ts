48afab419966a535039ebda8b01c7660ee2b559a
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-27 11:59:41 +0530

    Add: database connection

---
+++ src/db/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+// function to connect to the database
+async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    const connection = mongoose.connection;
+
+    // on successful connection
+    connection.on('connected', () => {
+      console.log('Mongo db conneceted');
+    });
+
+    // on error
+    connection.on('error', (error) => {
+      console.log('Unable to connect to mongo db');
+      process.exit();
+    });
+  } catch (error) {
+    console.log('soomething went wrong while connecting to db');
+    console.log(error);
+  }
+}
+
+export default connect;
